4a579b30e62aa10e90359344f5278b9ab3a6dbfa
Surfer5437 <livingstonebrandon5437@gmail.com>
2023-06-05 22:40:00 -0400

    init sync

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 /** Common config for bookstore. */
 
 
-let DB_URI = `postgresql://`;
+let DB_URI = `{
+  host: 'localhost',
+  user: 'livin',
+  port:5432,
+  password: 'rootUser',
+  database:'messagely'
+}`;
 
-if (process.env.NODE_ENV === "test") {
-  DB_URI = `${DB_URI}/books-test`;
-} else {
-  DB_URI = process.env.DATABASE_URL || `${DB_URI}/books`;
-}
+
 
 
 module.exports = { DB_URI };
